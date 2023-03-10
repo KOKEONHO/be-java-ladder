bb5637a9ff249d05c2feacab278d2d6f763902a6
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-10 16:17:57 +0900

    Feat: Validator.java 기능 추가
    Validator.java 내에 사용자가 입력한 참가자의 이름들이 0자 초과 6자 미만인지 검증하는 기능 추가 및 만약 이 범위 내에 속하지 않는 이름이 존재 시 반복하여 사용자의 입력을 요구하고 출력하는 기능 추가

--- src/main/java/kr/codesquad/model/Validator.java
+++ src/main/java/kr/codesquad/model/Validator.java
@@ -1,9 +1,37 @@
 package kr.codesquad.model;
 
+import kr.codesquad.view.OutputView;
+
 public class Validator {
 
+    private final OutputView outputView;
+
+    public Validator(OutputView outputView) {
+        this.outputView = outputView;
+    }
+
     public boolean checkUserInputPlayersNameLength(String playersName) {
-        
+        boolean whetherValidateName = true;
+        String[] participants = turnStringIntoStringArray(playersName);
+        whetherValidateName = isWhetherValidateName(whetherValidateName, participants);
+        if (!whetherValidateName) {
+            throw new IllegalArgumentException();
+        }
+        return whetherValidateName;
+    }
+
+    private boolean isWhetherValidateName(boolean whetherValidateName, String[] participants) {
+        for (int i = 0; i < participants.length && whetherValidateName; i++) {
+            whetherValidateName = checkWhetherPlayersNameLengthValidate(participants[i]);
+        }
+        return whetherValidateName;
+    }
+
+    private boolean checkWhetherPlayersNameLengthValidate(String name) {
+        if (name.length() < 6 && name.length() > 0) {
+            return true;
+        }
+        return false;
     }
 
     private String[] turnStringIntoStringArray(String playersName) {
