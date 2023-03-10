0e272f5ae6ce8e199336dd9ec346cde38af1bee3
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-10 16:16:17 +0900

    Feat: OutputView.java 기능 추가
    사용자가 입력한 참가자들의 이름이 5글자를 초과할 경우 출력할 때 필요한 예외 문구를 출력하는 기능 추가

--- src/main/java/kr/codesquad/view/OutputView.java
+++ src/main/java/kr/codesquad/view/OutputView.java
@@ -49,6 +49,10 @@ public class OutputView {
         globalPrintln("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
     }
 
+    public void printInvalidPlayersName() {
+        globalPrintln("[ERROR] 5글자 이하의 이름만 입력해주세요!!!");
+    }
+
     public void printLadder(String ladder) {
         globalPrintln(ladder);
     }
