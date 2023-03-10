436d8d7840390de66dbed5cffd08bed675b92e6c
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-10 14:04:26 +0900

    Feat: Validator.java 기능 추가
    사용자의 입력을 통해 받은 사다리 게임의 참가자들의 이름을 String에서 String[]로 변환하는 기능 추가

--- src/main/java/kr/codesquad/model/Validator.java
+++ src/main/java/kr/codesquad/model/Validator.java
@@ -1,5 +1,16 @@
 package kr.codesquad.model;
 
 public class Validator {
-    
+
+    public boolean checkUserInputPlayersNameLength(String playersName) {
+        
+    }
+
+    private String[] turnStringIntoStringArray(String playersName) {
+        String[] participants = playersName.split(",");
+        for (int i = 0; i < participants.length; i++) {
+            participants[i] = participants[i].trim();
+        }
+        return participants;
+    }
 }
