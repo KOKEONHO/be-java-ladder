b5489bf04810d2a0bd91af67d796c9de4718fcff
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-10 16:11:49 +0900

    Refactor: Application.java
    Validator.java가 생성됨에 따른 생성자 수정 및 OutputView의 객체를 LadderGameController의 객체보다 앞서 생성함으로써 LadderGameController의 객체와 Validator의 객체에 같은 OutputView의 객체가 넘겨질 수 있게 코드 수정

--- src/main/java/kr/codesquad/Application.java
+++ src/main/java/kr/codesquad/Application.java
@@ -3,13 +3,14 @@ package kr.codesquad;
 import kr.codesquad.controller.LadderGameController;
 import kr.codesquad.model.Converter;
 import kr.codesquad.model.LadderMaker;
+import kr.codesquad.model.Validator;
 import kr.codesquad.view.InputView;
 import kr.codesquad.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
-
-        LadderGameController ladderGameController = new LadderGameController(new InputView(), new OutputView(), new LadderMaker(), new Converter());
+        OutputView outputView = new OutputView();
+        LadderGameController ladderGameController = new LadderGameController(new InputView(), outputView, new LadderMaker(), new Converter(), new Validator(outputView));
         ladderGameController.ladderGameStart();
     }
 }
