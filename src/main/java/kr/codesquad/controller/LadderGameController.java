7ff975a0a6bcd9ebae7fddf9304d72c1799448cb
KOKEONHO <rhrjsgh97@gmail.com>
2023-03-10 16:14:06 +0900

    Refactor: LadderGameController.java 코드 수정
    Validator.java 생성에 따른 생성자 부분의 매개 변수 코드 수정 및 메서드 10줄 제한 준수를 위해 사용자의 입/출력 부분의 메서드를 promptValidPlayersName() 메서드로 분리하고 예외를 처리하는 부분 코드 수정

--- src/main/java/kr/codesquad/controller/LadderGameController.java
+++ src/main/java/kr/codesquad/controller/LadderGameController.java
@@ -3,6 +3,7 @@ package kr.codesquad.controller;
 import kr.codesquad.model.Converter;
 import kr.codesquad.model.Ladder;
 import kr.codesquad.model.LadderMaker;
+import kr.codesquad.model.Validator;
 import kr.codesquad.view.InputView;
 import kr.codesquad.view.OutputView;
 
@@ -12,20 +13,36 @@ public class LadderGameController {
     private final OutputView outputView;
     private final LadderMaker ladderMaker;
     private final Converter converter;
+    private final Validator validator;
 
-    public LadderGameController(InputView inputView, OutputView outputView, LadderMaker ladderMaker, Converter converter) {
+    public LadderGameController(InputView inputView, OutputView outputView, LadderMaker ladderMaker, Converter converter, Validator validator) {
         this.inputView = inputView;
         this.outputView = outputView;
         this.ladderMaker = ladderMaker;
         this.converter = converter;
+        this.validator = validator;
     }
 
     public void ladderGameStart() {
-        outputView.printAskNameOfPlayers();
-        String playersName = inputView.userInputPlayersName();
+        String playersName = promptValidPlayersName();
         outputView.printAskHeightOfLadder();
         int heightOfLadder = inputView.userInputHeightOfLadder();
         Ladder ladder = ladderMaker.createLadder(playersName, heightOfLadder);
         outputView.printLadder(converter.convertLadderToString(ladder));
+    }
+
+    private String promptValidPlayersName() {
+        boolean checkWhetherPlayersNameValid = false;
+        String userInputPlayersName = "";
+        while (!checkWhetherPlayersNameValid) {
+            try {
+                outputView.printAskNameOfPlayers();
+                userInputPlayersName = inputView.userInputPlayersName();
+                checkWhetherPlayersNameValid = validator.checkUserInputPlayersNameLength(userInputPlayersName);
+            } catch (IllegalArgumentException illegalArgumentException) {
+                outputView.printInvalidPlayersName();
+            }
+        }
+        return userInputPlayersName;
     }
 }
